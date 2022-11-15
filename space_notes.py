49f281d34f55b132195f7d883b0c41d0aead1b59
bulferJ <117784062+bulferJ@users.noreply.github.com>
2022-11-15 07:03:34 -0600

    Add files via upload

---
+++ space_notes.py
@@ -0,0 +1,19 @@
+#create orc_dead.xcf in gimp (split in half with red paint)
+
+#fix orc health bar 
+#if health bar = 0, game_over, death sound, and blit(orc_dead, orc_rect) for 2 sec
+#add game music
+
+#add sp health bar
+#if laser_rect.colliderect(orc_rect), highlight orc red
+
+#add enemy attack mechanic
+
+
+
+x = 5
+
+def test():
+    print(x)
+
+test()
